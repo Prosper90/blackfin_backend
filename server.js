9e05893a70ba2ede5b892d3b7b37832576559d93
Prosper <nkonyeprospero@gmail.com>
2024-11-03 13:23:37 +0100

    fixing cors

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 //parse application/json
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({ origin: ["https://blackfin.netlify.app"] }));
 
 app.get(`/info/:address`, async function (req, res, next) {
   try {
